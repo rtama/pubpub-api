b3d708acd66359cbc7f6d1e28098e6e6bdac794e
Travis Rich <travis.s.rich@gmail.com>
2016-12-19 15:06:50 -0500

    Update labels route to get a specific label

--- routes/labels/labels.js
+++ routes/labels/labels.js
@@ -2,13 +2,30 @@ import app from '../../server';
 import { Pub, Label, Contributor, JournalAdmin, PubLabel } from '../../models';
 
 export function getLabels(req, res, next) {
-	// Return all global labels
-	Label.findAll({
-		where: { journalId: null, userId: null, pubId: null },
-		attributes: ['id', 'title', 'color']
+	// Return a single label and the associated pubs
+
+	const whereParams = {};
+	Object.keys(req.query).map((key)=> {
+		if (['id', 'title', 'journalId', 'userId', 'pubId'].indexOf(key) > -1) {
+			whereParams[key] = req.query[key];
+		} 
+	});
+
+	if (whereParams.title && !whereParams.userId) { whereParams.userId = null; }
+	if (whereParams.title && !whereParams.journalId) { whereParams.journalId = null; }
+	if (whereParams.title && !whereParams.pubId) { whereParams.pubId = null; }
+
+	// const whereParameters = Object.keys(queryParams).length
+	// 	? queryParams
+	// 	: { journalId: null, userId: null, pubId: null };
+
+	Label.findOne({
+		where: whereParams,
+		attributes: ['id', 'title', 'color', 'journalId', 'userId', 'pubId'],
+		include: [{ model: Pub, as: 'pubs' }],
 	})
 	.then(function(labelsData) {
-		if (!labelsData || !labelsData.length) { return res.status(500).json('Labels not found'); }
+		if (!labelsData) { return res.status(500).json('Label not found'); }
 		return res.status(201).json(labelsData);
 	})
 	.catch(function(err) {
