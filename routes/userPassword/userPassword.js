c72b54ccfa9932200ea5d7a0fd39e82e9757a3b5
Travis Rich <travis.s.rich@gmail.com>
2016-12-27 11:56:24 -0500

    Restructuring update functions and adding wrapper for getReset route.

--- routes/userPassword/userPassword.js
+++ routes/userPassword/userPassword.js
@@ -1,34 +1,37 @@
-import passport from 'passport';
-// import cookie from 'cookie';
-// import languageParser from 'accept-language-parser';
-// import Promise from 'bluebird';
+import Promise from 'bluebird';
 import app from '../../server';
-
 import { User } from '../../models';
 
-
-// If a User is authenticated then change their password
-export function setPassword(req, res) {
+// If a User is authenticated with reset hash then change their password
+export function postPassword(req, res) {
 	const hash = req.body.resetHash;
 	const username = req.body.username;
+	const currentTime = Date.now();
 	User.findOne({
 		where: { resetHash: hash, username: username },
-	}).then(function(user) {
-		const currentTime = Date.now();
-
-		if (!user){ throw new Error('User doesn\'t exist'); }
-
+	})
+	.then(function(user) {
+		if (!user) { throw new Error('User doesn\'t exist'); }
 		if (user.resetHashExpiration < currentTime) { throw new Error('Hash is expired'); }
 
-		// Update user
-		user.setPassword(req.body.password, function() {
-			user.resetHash = '';
-			user.resetHashExpiration = currentTime;
-			user.save();
-			return res.status(200).json('success');
+		// Promisify the setPassword function, and use .update to match API convention
+		const setPassword = Promise.promisify(user.setPassword, { context: User });
+		return setPassword(req.body.password);
+	})
+	.then(function(passwordResetData) {
+		const updateData = {
+			resetHash: '',
+			resetHashExpiration: currentTime,
+		};
+		return User.update(updateData, {
+			where: { username: username },
 		});
-	}).catch(function(err) {
+	})
+	.then(function(updatedCount) {
+		return res.status(200).json('success');
+	})
+	.catch(function(err) {
 		return res.status(401).json(err.message);
 	});
 }
-app.post('/user/password', setPassword);
+app.post('/user/password', postPassword);
--- routes/userPasswordReset/userPasswordReset.js
+++ routes/userPasswordReset/userPasswordReset.js
@@ -1,46 +1,49 @@
-import passport from 'passport';
-// import cookie from 'cookie';
-// import languageParser from 'accept-language-parser';
-// import Promise from 'bluebird';
+/* eslint-disable no-param-reassign */
 import app from '../../server';
 
 import { User } from '../../models';
 import { generateHash } from '../../utilities/generateHash';
 import { sendResetEmail } from '../../utilities/sendResetEmail';
 
-// Contains GET and post
-// POST to create the request + Hash
-// GET to check the hash
+export function getReset(req, res) {
+	// This route should be called when the frontend route with /:has loads
+	// We want to check that the hash is valid before the user submits the new password.
+	// They should not see a reset form if the hash is invalid.
+	// See getSignUp in /signUp/signUp.js
 
-export function requestReset(req, res) {
-	// const authenticatedUserAttributes = ['email'];
-	// const resetPasswordData = {};
-	// const success = false;
+	return res.status(200).json(true);
+}
+app.get('/user/password/reset', getReset);
 
+export function postReset(req, res) {
 	User.findOne({
-		where:{ email: req.body.email }
+		where: { email: req.body.email }
 	}).then(function(user) {
-		if (!user ){ throw new Error("User doesn't exist"); }
-
+		if (!user) { throw new Error("User doesn't exist"); }
 
 		const resetHash = generateHash();
-		const expiration = Date.now() + 1000 * 60 * 60 * 24; // Expires in 24 hours.
+		const expiration = Date.now() + (1000 * 60 * 60 * 24); // Expires in 24 hours.
 
-		user.resetHash = resetHash;
-		user.resetHashExpiration = expiration;
+		// Use User.update rather than .save() to match convention with the rest of the API
+		const updateData = {
+			resetHash: resetHash,
+			resetHashExpiration: expiration,
+		};
+		return User.update(updateData, {
+			where: { id: user.pubId },
+			returning: true,
+		});
 
-		console.log(`New reset hash:\n\t${resetHash}\n`);
-
-		return user.save();
-	}).then(function(user) {
-		// Send reset email
-		return sendResetEmail({ email: user.email, hash: user.resetHash, username: user.username });
+	}).then(function(updatedUserData) {
+		const updatedUser = updatedUserData[1][0];
+		return sendResetEmail({ email: updatedUser.email, hash: updatedUser.resetHash, username: updatedUser.username });
 	})
-	.then(function(){
-		return res.status(200).json({});
+	.then(function() {
+		// Always return something. Never an empty object. Makes debugging pretty hard.
+		return res.status(200).json(true);
 	})
 	.catch(function(err) {
 		return res.status(401).json(err.message);
 	});
 }
-app.post('/user/password/reset', requestReset);
+app.post('/user/password/reset', postReset);
