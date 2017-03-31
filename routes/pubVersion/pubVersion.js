526fb83f50000c8d7cfac3e40aae58f52a762467
Travis Rich <travis.s.rich@gmail.com>
2017-03-31 16:40:26 -0400

    Fix name bug in uploadFile

--- routes/pubVersion/pubVersion.js
+++ routes/pubVersion/pubVersion.js
@@ -25,8 +25,8 @@ export function postVersion(req, res, next) {
 
 	// Separate old files (ones already parsed on the PubPub end) from new ones
 	const files = req.body.files || [];
-	const oldFiles = files.filter((file)=> { return file.id !== undefined; });
-	const newFiles = files.filter((file)=> { return file.id === undefined; });
+	const oldFiles = files.filter((file)=> { return file.id; });
+	const newFiles = files.filter((file)=> { return !file.id; });
 
 	let newVersionId;
 
@@ -66,7 +66,7 @@ export function postVersion(req, res, next) {
 
 		const newFilesWithContent = newFiles.map((file, index)=> {
 			filesHashes.push(promiseResults[index].hash);
-			return { ...file, ...promiseResults[index], pubId: req.body.pubId };
+			return { ...file, ...promiseResults[index], pubId: req.body.pubId, updatedAt: undefined, createdAt: undefined };
 		});
 
 		// To generate the version hash, take the hash of all the files, sort them alphabetically, concatenate them, and then hash that string.
